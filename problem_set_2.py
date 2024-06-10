0f9c51abd9745657a848279682d40dcae6d3980b
Ida Litzen <Iml3679@nyu.edu>
2024-06-10 15:49:22 +0200

    exam practice. done

--- problem_set_2.py
+++ problem_set_2.py
@@ -30,4 +30,35 @@ def weather_helper():
     2. Do not print anything more than what is requested in the instructions.
     4. The capitalization of the user's responses must not matter to the outcome of the program.
   """
+  current_temp=int(input("Enter in the current temperature in degrees Farenheit: "))
 
+  if current_temp < -70 or current_temp > 134:
+    print("Invalid temperature!")
+  else:
+    if current_temp<40:
+      snowing=input("Is it snowing? ")
+      snowing=snowing.strip().lower()
+      if snowing in ["yes", "yeah", "yup"]:
+        jacket=input("Are you wearing a warm jacket? ")
+        jacket=jacket.strip().lower()
+        if jacket in ["yes", "yeah", "yup"]:
+          print("Glad to hear you're dressed appropriately!")
+        else:
+          print("What were you thinking when you left home today?!")
+      else:
+        rain=input("Is it raining? ")
+        rain=rain.strip().lower()
+        if rain in ["yes", "yeah", "yup"]:
+          umbrella=input("Do you have an umbrella? ")
+          umbrella=umbrella.strip().lower()
+          if umbrella in ["yes", "yeah", "yup"]:
+            print("Good job staying dry!")
+          else:
+            print("You must enjoy getting wet!")
+    elif current_temp>90:
+      air=input("Do you have air conditioning? ")
+      air=air.strip().lower()
+      if air in ["yes", "yeah", "yup"]:
+        print("Stay cool indoors.")
+      else:
+        print("I hope you have a fan.")
--- problem_set_3.py
+++ problem_set_3.py
@@ -13,6 +13,8 @@ import random
 # The function must return a random integer between these two values, inclusive.
 # Use the function random.randint() to generate the pseudo-random number.
 
+def get_random_int(min_value, max_value):
+    return random.randint(min_value, max_value)
 
 ##--------------------- Function #2 ---------------------##
 # Define a function named 'get_guess'.
@@ -23,7 +25,19 @@ import random
 # If the user has guessed the random integer correctly, this function returns a boolean True.
 # If the user has guessed incorrectly, this function returns a boolean False.
 
-
+def get_guess(max_value):
+    guess_human=(input(f"Guess a number between 1 and {max_value} :"))
+    if not guess_human.isdigit():
+        return -1
+    guess_human=int(guess_human)
+    random_int=get_random_int(1, max_value)
+    if guess_human < 1 or guess_human>max_value:
+        return -1
+    if guess_human == random_int:
+        return True
+    else:
+        return False
+    
 ##--------------------- Function #3 ---------------------##
 # Define a function named 'play_game'.
 # This function does not accept any arguments.
@@ -32,3 +46,20 @@ import random
 # If at any time, the user enters an invalid response, the program immediately prints out the text, "Invalid response!" and does not print out anything further.
 # At the end, the function, assuming the user has entered all valid guesses, the program prints out the percent of guesses that user guessed correctly, following the format: "You guessed 75% of the random numbers correctly."
 
+def play_game():
+    correct_guesses = 0
+    total_guesses = 4
+
+    for _ in range(total_guesses):
+        result = get_guess(5)
+        if result is True:
+            print("Correct!")
+            correct_guesses += 1
+        elif result is False:
+            print("Wrong!")
+        elif result == -1:
+            print("Invalid response!")
+            return
+
+    percentage_correct = (correct_guesses / total_guesses) * 100
+    print(f"You guessed {percentage_correct:.0f}% of the random numbers correctly.")
